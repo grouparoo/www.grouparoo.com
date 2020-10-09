9314f9f0bfc58a97f3432710eade07b858538719
Andy Jih <andy.y.jih@gmail.com>
2020-10-09 13:58:14 -0700

    New CTA

--- components/home/integrationsSection.tsx
+++ components/home/integrationsSection.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import IntegrationCard from "./integrationCard";
 import Link from "next/link";
 
@@ -18,28 +18,21 @@ export default function IntegrationsSection() {
         </Row>
 
         <Row>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Mailchimp"
               slug="mailchimp"
               image="mailchimp.svg"
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
-            <IntegrationCard
-              name="Sailthru"
-              slug="sailthru"
-              image="sailthru.png"
-            />
-          </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Postgres"
               slug="postgres"
               image="postgres.svg"
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="BigQuery"
               slug="bigquery"
@@ -48,69 +41,60 @@ export default function IntegrationsSection() {
           </Col>
         </Row>
         <Row>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard name="MySQL" slug="mysql" image="mysql.png" />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
-              name="CSV"
-              slug="csv"
-              image="csv.svg"
+              name="Salesforce"
+              slug="salesforce"
+              image="salesforce.jpg"
               showLink={false}
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Hubspot"
               slug="hubspot"
               image="hubspot.svg"
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
-            <IntegrationCard
-              name="Google Sheets"
-              slug="google-sheets"
-              image="google-sheets.png"
-              showLink={false}
-            />
-          </Col>
         </Row>
         <Row>
-          <Col lg={1} md={1} sm={0} xs={0} />
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Redshift"
               slug="redshift"
               image="redshift.svg"
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Zendesk"
               slug="zendesk"
               image="zendesk.svg"
             />
           </Col>
-          <Col lg={3} md={3} sm={6} xs={6}>
+          <Col lg={4} md={4} sm={6} xs={6}>
             <IntegrationCard
               name="Marketo"
               slug="marketo"
               image="marketo.png"
             />
           </Col>
-          <Col lg={1} md={1} sm={0} xs={0} />
         </Row>
 
         <Row>
           <Col>
-            <p />
-            <p>And many more on the way!</p>
-            <p>
-              <Link href="/meet">
-                <a>Let us know </a>
-              </Link>
-              what products you want to connect with.
-            </p>
+            <br />
+            <br />
+            <Button
+              variant="outline-primary"
+              href="/integrations"
+              className="integrations-button"
+            >
+              See all of our available integrations
+            </Button>
           </Col>
         </Row>
       </Container>
