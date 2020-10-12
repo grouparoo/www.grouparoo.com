c69755be3df89178f3ab1a701f3f24e41b270df3
Evan Tahler <evantahler@gmail.com>
2020-10-12 12:19:19 -0700

    upgrading page

--- utils/blogPosts.ts
+++ utils/blogPosts.ts
@@ -1,4 +1,4 @@
 //@ts-ignore
-//<<random-timestamp>> 2020-10-01T18:05:57.438Z
+//<<random-timestamp>> 2020-10-12T19:08:25.519Z
 import { frontMatter as blogFiles } from "../pages/blog/*.mdx";
 export default blogFiles;
--- utils/docPosts.ts
+++ utils/docPosts.ts
@@ -1,4 +1,4 @@
 //@ts-ignore
-//<<random-timestamp>> 2020-10-01T18:05:57.444Z
+//<<random-timestamp>> 2020-10-12T19:08:25.520Z
 import { frontMatter as docFiles } from "../pages/docs/**/*.mdx";
 export default docFiles;
