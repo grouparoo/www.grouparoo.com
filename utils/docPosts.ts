607df09f73279aaf1ff3f42e7aa02bbc0c11a04c
Evan Tahler <evantahler@gmail.com>
2020-10-16 15:47:33 -0700

    Using Typescript to create a Robust API between your frontend and backend

--- utils/docPosts.ts
+++ utils/docPosts.ts
@@ -1,4 +1,4 @@
 //@ts-ignore
-//<<random-timestamp>> 2020-10-13T23:44:48.415Z
+//<<random-timestamp>> 2020-10-16T22:30:20.316Z
 import { frontMatter as docFiles } from "../pages/docs/**/*.mdx";
 export default docFiles;
