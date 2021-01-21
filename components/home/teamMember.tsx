a500fc4bd5df545b82467cef8d78819ccf0f90c9
Sean C Davis <sean.davis@grouparoo.com>
2021-01-21 07:10:02 -0500

    Fix layout of social icons

--- components/home/teamMember.tsx
+++ components/home/teamMember.tsx
@@ -28,18 +28,18 @@ export default function TeamMember({
           </p>
         </Col>
       </Row>
-      <Row className="justify-content-md-center">
-        <Col xs={2}>
+      <Row className="justify-content-center">
+        <Col xs={2} className="px-1">
           <a href={github} target="_blank" rel="noreferrer">
             <SocialIconGrayscale name="github" />
           </a>
         </Col>
-        <Col xs={2}>
+        <Col xs={2} className="px-1">
           <a href={twitter} target="_blank" rel="noreferrer">
             <SocialIconGrayscale name="twitter" />
           </a>
         </Col>
-        <Col xs={2}>
+        <Col xs={2} className="px-1">
           <a href={linkedIn} target="_blank" rel="noreferrer">
             <SocialIconGrayscale name="linkedin" />
           </a>
