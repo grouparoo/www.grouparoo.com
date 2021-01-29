8b529cb4fee219d52bf55745f78d6a28e4a16224
Sean C Davis <sean.davis@grouparoo.com>
2021-01-29 16:24:05 -0500

    Update structure of table of contents to customize title

--- components/docs/tableOfContents.tsx
+++ components/docs/tableOfContents.tsx
@@ -16,23 +16,23 @@ export function TableOfContents({ docs }) {
         <Card.Header>Grouparoo Docs</Card.Header>
         <Card.Body>
           <small>
-            {Object.keys(tableOfContents).map((section) => {
+            {Object.entries(tableOfContents).map(([id, section]) => {
               return (
-                <Fragment key={`toc-section-${section}`}>
-                  <Link href={`/docs/${section}`}>
+                <Fragment key={`toc-section-${id}`}>
+                  <Link href={`/docs/${section.path}`}>
                     <a
                       style={{
                         color:
-                          router.asPath === `/docs/${section}`
+                          router.asPath === `/docs/${section.path}`
                             ? highlightColor
                             : undefined,
                       }}
                     >
-                      <strong>{titleize(section)}</strong>
+                      <strong>{section.title}</strong>
                     </a>
                   </Link>
                   <ul>
-                    {tableOfContents[section].map((entry) => (
+                    {section.children.map((entry) => (
                       <li key={`toc-${section}-${entry.title}`}>
                         <Link href={entry.path}>
                           <a
@@ -60,13 +60,21 @@ export function TableOfContents({ docs }) {
   );
 }
 
+interface NavItem {
+  title: string;
+  path: string;
+  children: NavItem[];
+}
+
 export function tableOfContentsFromEntries(docs) {
-  const sections: { [section: string]: { title: string; path: string }[] } = {
-    "try-now": [],
-    deployment: [],
-    guides: [],
-    development: [],
-    community: [],
+  const sections: { [section: string]: NavItem } = {
+    "try-now": { title: "Try Now!", path: "try-now", children: [] },
+    installation: { title: "Installation", path: "installation", children: [] },
+    //
+    deployment: { title: "Deployment", path: "deployment", children: [] },
+    guides: { title: "Guides", path: "guides", children: [] },
+    development: { title: "Development", path: "development", children: [] },
+    community: { title: "Community", path: "community", children: [] },
   };
 
   const externalLinks = {
@@ -86,14 +94,17 @@ export function tableOfContentsFromEntries(docs) {
     const pathParts = entry.path.split("/");
     if (pathParts.length > 3) {
       const section = pathParts[2];
-      if (!sections[section]) sections[section] = [];
-      sections[section].push(entry);
+      if (!sections[section]) {
+        sections[section] = { title: section, path: section, children: [] };
+      }
+      if (!sections[section].children) sections[section].children = [];
+      sections[section].children.push(entry);
     }
   });
 
   for (const section in externalLinks) {
     for (const k in externalLinks[section]) {
-      sections[section].push(externalLinks[section][k]);
+      sections[section].children.push(externalLinks[section][k]);
     }
   }
 
