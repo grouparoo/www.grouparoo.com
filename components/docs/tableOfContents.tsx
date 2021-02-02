2930d644b1276a2aeb9df70ccf35b8260adb22b1
Sean C Davis <scdavis41@gmail.com>
2021-02-02 06:08:19 -0500

    Support an infinite number of nav tiers in docs

--- components/docs/tableOfContents.tsx
+++ components/docs/tableOfContents.tsx
@@ -4,9 +4,41 @@ import Link from "next/link";
 import { Card } from "react-bootstrap";
 import navItems from "../../data/docs-nav";
 
+const highlightColor = "#fd7e14";
+
+const NavListItem = ({ item }) => {
+  const router = useRouter();
+
+  return (
+    <li>
+      <Link href={item.path}>
+        <a
+          style={{
+            color: router.asPath === item.path ? highlightColor : undefined,
+          }}
+        >
+          {item.title}
+        </a>
+      </Link>
+      <NavList items={item.children} />
+    </li>
+  );
+};
+
+const NavList = ({ items }) => {
+  if (!items || items.length === 0) return null;
+
+  return (
+    <ul>
+      {items.map((item, idx) => (
+        <NavListItem key={idx} item={item} />
+      ))}
+    </ul>
+  );
+};
+
 export function TableOfContents() {
   const router = useRouter();
-  const highlightColor = "#fd7e14";
 
   return (
     <>
@@ -29,24 +61,7 @@ export function TableOfContents() {
                       <strong>{section.title}</strong>
                     </a>
                   </Link>
-                  <ul>
-                    {section.children.map((entry) => (
-                      <li key={`toc-${section}-${entry.title}`}>
-                        <Link href={entry.path}>
-                          <a
-                            style={{
-                              color:
-                                router.asPath === entry.path
-                                  ? highlightColor
-                                  : undefined,
-                            }}
-                          >
-                            {entry.title}
-                          </a>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
+                  <NavList items={section.children} />
                 </Fragment>
               );
             })}
