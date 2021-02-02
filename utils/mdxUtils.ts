6c3ad58b926bf806bb274f9dd3883eb5c11af959
Sean C Davis <sean.davis@grouparoo.com>
2021-02-02 16:40:21 -0500

    Build breadcrumbs object in the MDX utils.

--- utils/mdxUtils.ts
+++ utils/mdxUtils.ts
@@ -8,6 +8,8 @@ import matter from "gray-matter";
 import rehypePrism from "@mapbox/rehype-prism";
 import externalLinks from "remark-external-links";
 
+import { titleize } from "../utils/inflectors";
+
 const dirname = __dirname;
 let pagesDir;
 if (!dirname || dirname === "/") {
@@ -47,6 +49,7 @@ export async function loadMdxFilePath(fullPath, components) {
   return Object.assign(
     {},
     { source: mdxSource, frontMatter: data },
+    getBreadcrumbs(fullPath),
     getMetadata(fullPath)
   );
 }
@@ -81,4 +84,46 @@ export async function getStaticMdxPaths(dirParts: string[], depth = 1) {
     paths,
     fallback: false,
   };
+}
+
+/**
+ * Given an absolute path to a page, build an array of breadcrumb objects. Look
+ * for MDX files at every segment in the path to extract the title. If they
+ * don't exist, use the path segment to generate the title.
+ *
+ * @param fullPath Path to the file
+ */
+export function getBreadcrumbs(fullPath: string) {
+  // The path the to the page (e.g. "/docs/deployment/docker")
+  const fullPagePath = fullPath.replace(pagesDir, "").replaceAll(".mdx", "");
+  // Split the path into individual segments. The first one is an empty string
+  // because we would have had a leading slash.
+  const pathParts = fullPagePath.split("/");
+  // Initialize breadcrumbs object.
+  let breadcrumbs = [];
+  // Step through each of the segments in the path, starting with the empty one.
+  pathParts.reduce((prev, curr) => {
+    // Path to this segment, from the pages directory.
+    const pagePath = `${prev}/${curr}`;
+    // Absolute path to the file we're looking for.
+    const filePath = `${path.join(pagesDir, pagePath)}.mdx`;
+    // The default title is a titleized version of the segment, in case we can't
+    // find the file.
+    let title = titleize(curr);
+    // If the file does exist ...
+    if (fs.existsSync(filePath)) {
+      // Read it.
+      const content = fs.readFileSync(filePath);
+      // Extract the frontmattter.
+      const { data } = matter(content);
+      // Look for a title value, and set the title if it's there.
+      if (data.title) title = data.title;
+    }
+    // Add the breadcrumb object.
+    breadcrumbs.push({ title, path: pagePath });
+    // Return the path to the next iteration so we can build on top of it.
+    return pagePath;
+  });
+  // Return the breadcrumbs object.
+  return { breadcrumbs };
 }
