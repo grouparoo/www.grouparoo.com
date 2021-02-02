4af3062c7f326aecbc76152fe406ffc80e9f2473
Sean C Davis <sean.davis@grouparoo.com>
2021-02-02 15:10:49 -0500

    Delete support test

--- __tests__/pages/support.tsx
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import Support from "../../pages/support";
-
-describe("page/support", () => {
-  test("renders the page", () => {
-    const page = shallow(<Support />);
-    expect(page.html()).toContain("Github Issues");
-  });
-});
