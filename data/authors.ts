1cb7de8e91507656345e794fdaebe98d224efe51
Sean C Davis <scdavis41@gmail.com>
2021-02-09 05:59:45 -0500

    [Blost Post] Node.js Memory Error on Apple M1 (#181)
    * Add Sean as an author
    
    * Add post: Node.js Memory Error on Mac Using M1
    
    * Fix images for authors
    
    * Add image and quick note on running in compatibility mode
    
    * Updating things automatically
    
    * Updating things automatically
    
    * Account for author images that aren't PNGs
    
    * Update: Node.js Memory Error on Mac Using M1
    
    * Updating things automatically
    
    Co-authored-by: Grouparoo Bot <hello@grouparoo.com>

--- data/authors.ts
+++ data/authors.ts
@@ -1,11 +1,13 @@
-const Authors: Array<{
+export interface Author {
   name: string;
   slug: string;
   image: string;
   url: string;
   twitter: string;
   description: string;
-}> = [
+}
+
+const Authors: Author[] = [
   {
     name: "Evan Tahler",
     slug: "evan_tahler",
@@ -41,6 +43,15 @@ const Authors: Array<{
     twitter: "@not_woods",
     description:
       "Tiger is a guest writer working on Microsoft Edge. He has previously worked on PWA support in Firefox for Android, web projects for Google Chrome and Yelp, and studies at the University of British Columbia. In his spare time, Tiger searches for geocaches with his partner.",
+  },
+  {
+    name: "Sean C Davis",
+    slug: "seancdavis",
+    image: "sean_c_davis.jpg",
+    url: "https://www.seancdavis.com/",
+    twitter: "@seancdavis29",
+    description:
+      "Sean is a full-stack developer working on Grouparoo, an open source data framework that easily connects your data to business tools. Sean is a tinkerer and a teacher who enjoys long walks in the woods, spending time with his family, baking bread, playing music, and eating sandwiches.",
   },
 ];
 
