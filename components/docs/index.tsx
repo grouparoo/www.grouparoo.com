fb10bc3bda964ca3b4d0f90cde49dd8b1afd36b5
Evan Tahler <evan.tahler@grouparoo.com>
2021-02-11 08:57:31 -0800

    docs/installation/plugins (#205)
    * plugins docs page
    
    * copy updates
    
    * Updating things automatically
    
    * version before description
    
    * Updates from Andy
    
    * Add link to plugins from installation page
    
    Co-authored-by: Grouparoo Bot <hello@grouparoo.com>

--- components/docs/index.tsx
+++ components/docs/index.tsx
@@ -3,6 +3,7 @@ import Image from "./image";
 import HavingProblems from "./havingProblems";
 import RuleOpsTable from "./ruleOpsTable";
 import CLICommand from "./CLICommands";
+import PluginsList from "./pluginsList";
 
 // The exported components are available for use in docs MDX files.
 export {
@@ -12,6 +13,7 @@ export {
   Image,
   RuleOpsTable,
   CLICommand,
+  PluginsList,
   Tabs,
   Tab,
 };
