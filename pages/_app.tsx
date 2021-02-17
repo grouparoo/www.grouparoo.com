0ae8257ea3dfb4ad05cc483b0a8b1118eb17475d
Sean C Davis <scdavis41@gmail.com>
2021-02-17 06:17:28 -0500

    Send campaign data with meet form submission (#223)
    * Send campaign data with meet form submission
    Store current and previous URLs in session storage
    
    * Updating things automatically
    
    Co-authored-by: Grouparoo Bot <hello@grouparoo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,34 @@
 import "../scss/grouparoo.scss";
 import "../components/icons"; // this is needed to load the library
 import Layout from "../components/layouts/main";
-import Router from "next/router";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function GrouparooWWW(props) {
   const { Component } = props;
+  const router = useRouter();
 
-  // always scroll back to the top of the page when using Router.push()
-  Router.events.on("routeChangeComplete", () => {
-    window.scrollTo(0, 0);
-  });
+  useEffect(() => {
+    setCookies();
+
+    router.events.on("routeChangeComplete", scrollToTop);
+
+    return () => {
+      router.events.off("routeChangeComplete", scrollToTop);
+    };
+  }, [router.asPath]);
+
+  function setCookies() {
+    const storage = globalThis?.sessionStorage;
+    if (!storage) return;
+    const prevPath = storage.getItem("currentPath");
+    storage.setItem("prevPath", prevPath);
+    storage.setItem("currentPath", globalThis.location.pathname);
+  }
+
+  function scrollToTop() {
+    globalThis.scrollTo(0, 0);
+  }
 
   return (
     <Layout>
