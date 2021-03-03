f54ecc825b4d4a4f5c27bd8beb798afa323aa32b
Andy Jih <andy.y.jih@gmail.com>
2021-03-03 10:26:04 -0800

    Updated plugins structure (still a WIP)

--- data/plugins.ts
+++ data/plugins.ts
@@ -9,6 +9,26 @@ export const PluginData: Array<{
   category: string;
   badge: string;
   showLink: boolean;
+  packageName: string;
+  useCases: {
+    paragraphOne: string;
+    paragraphTwo: string;
+  };
+  useCasesList: string[];
+  dataModelTitle: string;
+  dataModelDescription: string[];
+  destinationProfile?: string;
+  destinationProperty?: string;
+  destinationGroup?: string;
+  otherPluginsHeading: string;
+  pluginScreenshots?: {
+    header: string;
+    description: string;
+    imageSrc: string;
+    imageAlt: string;
+    imageWidth: number;
+    imageHeight: number;
+  }[];
 }> = [
   {
     name: "CSV",
@@ -21,6 +41,7 @@ export const PluginData: Array<{
     category: "data tool",
     badge: "",
     showLink: false,
+    useCases: { intro: "" },
   },
   {
     name: "Facebook",
@@ -33,6 +54,7 @@ export const PluginData: Array<{
     category: "ad platform",
     badge: "",
     showLink: true,
+    useCases: { intro: "" },
   },
   {
     name: "Google BigQuery",
@@ -96,7 +118,8 @@ export const PluginData: Array<{
   },
   {
     name: "Mailchimp",
-    description: "",
+    description:
+      "Grouparoo's Mailchimp integration allows you to sync all of your customer data that lives in various sources into Mailchimp.",
     priority: 80,
     slug: "mailchimp",
     logo: "mailchimp.svg",
@@ -105,6 +128,52 @@ export const PluginData: Array<{
     category: "email marketing",
     badge: "popular",
     showLink: true,
+    packageName: "@grouparoo/mailchimp",
+    useCases: {
+      paragraphOne:
+        "Grouparoo is open source data infrastructure to keep customer data in-sync between your data warehouse and 3rd party tools such as Mailchimp.",
+      paragraphTwo:
+        "Grouparoo pulls customer data in real-time from all of your data sources and keeps that data synced to Mailchimp.",
+    },
+    useCasesList: [
+      "Automatically create Customers as new users sign up in your core product",
+      "Create new Merge Fields in Mailchimp without needing engineering help",
+      "Trigger email campaigns based on dynamic Audiences",
+      "Keep Merge Field values up-to-date automatically",
+    ],
+    dataModelTitle: "An Overview of Mailchimp’s Data Model and API",
+    dataModelDescription: [
+      "Trying to figure out how to integrate with Mailchimp? Here’s an overview for how Mailchimp defines customers.",
+      "The core object in Mailchimp is a Contact (also known as a Member). A Contact represents an individual person that lives on a List or Audience. Contacts have one required attribute, which is their email address.",
+      "Mailchimp calls attributes of a Contact a Merge Field (formerly a Merge Var). MailChimp pre-defines a number of Merge Fields like <code>FNAME</code> (first name) and <code>LNAME</code> (last name) for example, but you can define as many Merge Fields as you'd like using Grouparoo or in the Mailchimp website.",
+    ],
+    destinationProfile: "Mailchimp Contacts",
+    destinationProperty: "Mailchimp Merge Vars",
+    destinationGroup: "Mailchimp Tags",
+    otherPluginsHeading:
+      "Import your customer data from these tools into Mailchimp",
+    pluginScreenshots: [
+      {
+        header: "Define what customer data you want in Mailchimp",
+        description:
+          "Grouparoo is low-code, so non-technical team members can modify what data they send to different tools without needing engineering support.",
+        imageSrc:
+          "/images/home/integrations/mailchimp/grouparoo-to-mailchimp.png",
+        imageAlt: "Map data to Mailchimp",
+        imageWidth: 742,
+        imageHeight: 506,
+      },
+      {
+        header:
+          "Grouparoo keeps your customer data in Mailchimp updated in real-time",
+        description:
+          "Run campaigns in Mailchimp based on the Merge Fields and Tags that you've mapped from Grouparoo.",
+        imageSrc: "/images/home/integrations/mailchimp/mailchimp-audience.png",
+        imageAlt: "Mailchimp audience populated",
+        imageWidth: 742,
+        imageHeight: 518,
+      },
+    ],
   },
   {
     name: "Marketo",
@@ -120,7 +189,8 @@ export const PluginData: Array<{
   },
   {
     name: "MySQL",
-    description: "",
+    description:
+      "Grouparoo's MySQL integration allows you to sync all of your customer data that lives in your MySQL database with various destinations such as CRMs, Customer Support Tools, and Marketing Tools.",
     priority: 90,
     slug: "mysql",
     logo: "mysql.png",
@@ -129,6 +199,55 @@ export const PluginData: Array<{
     category: "database",
     badge: "",
     showLink: true,
+    packageName: "@grouparoo/mysql",
+    useCases: {
+      paragraphOne:
+        "Grouparoo is open source data infrastructure to keep your customer data in-sync with 3rd party tools.",
+      paragraphTwo:
+        "Grouparoo pulls customer data in real-time from your database, data warehouse, and other data sources.",
+    },
+    useCasesList: [
+      "Easily connect your production data with 3rd party tools",
+      "Create a unified customer definition in Grouparoo",
+      "Easily make cohorts and segments out of your MySQL data",
+      "Denormalize your database so you can easily send customer data to tools like CRMs, marketing automation, and email marketing",
+    ],
+    dataModelTitle: "An Overview of Grouparoo's Data Model",
+    dataModelDescription: [
+      "Grouparoo's core objects are Profiles and Profile Properties. These objects are defined based on your data such as the data in your MySQL DB. You can pull this data in with Grouparoo's help or you can write SQL queries directly.",
+      "Once you've defined these Profiles and Profile Properties, you can use Grouparoo to create dynamic segments and cohorts.",
+    ],
+    otherPluginsHeading: "Send your customer data from MySQL to these tools",
+    pluginScreenshots: [
+      {
+        header: "Easily connect your MySQL data",
+        description:
+          "Since Grouparoo is installed in your cloud infrastructure, you can easily point Grouparoo to any number of MySQL instances you may have running.",
+        imageSrc: "/images/home/integrations/mysql/add-mysql-app.png",
+        imageAlt: "Grouparoo MySQL settings",
+        imageWidth: 742,
+        imageHeight: 506,
+      },
+      {
+        header:
+          "Define Profile Properties without needing to write SQL queries",
+        description:
+          "Grouparoo is low-code, so less technical team members can create new Profile Properties without needing to work with engineers. They can pull the data they need when they want.",
+        imageSrc: "/images/home/integrations/mysql/mysql-table-mode.png",
+        imageAlt: "MySQL table source",
+        imageWidth: 742,
+        imageHeight: 806,
+      },
+      {
+        header: "But you can also write SQL if you want",
+        description:
+          "Some Profile Properties will require joins across tables or more complex queries. Use Grouparoo's Query mode to pull the data you need.",
+        imageSrc: "/images/home/integrations/mysql/mysql-query-mode.png",
+        imageAlt: "MySQL query source",
+        imageWidth: 742,
+        imageHeight: 506,
+      },
+    ],
   },
   {
     name: "Postgres",
---
+++ pages/integrations/[type]/[plugin].tsx
@@ -0,0 +1,293 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Image from "../../../components/Image";
+import IntegrationCard from "../../../components/home/integrationCard";
+import IntegrationMappings from "../../../components/home/integrationMappings";
+import IntegrationInstallation from "../../../components/home/integrationInstallation";
+import IntegrationSources from "../../../components/home/integrationSources";
+import IntegrationDestinations from "../../../components/home/integrationDestinations";
+import IntegrationsHead from "../../../components/home/integrationsHead";
+import GetStarted from "../../../components/home/getStarted";
+import BigArrow from "../../../components/bigArrow";
+import { PluginData } from "../../../data/plugins";
+
+export default function IntegrationsPage({ pageProps }) {
+  const { plugin, type }: { plugin: string; type: string } = pageProps;
+  const data = PluginData.find((e) => e.slug === plugin);
+
+  function useCaseList() {
+    const useCaseList = data.useCasesList.map((useCase, idx) => (
+      <li key={`useCase-${idx}`}>{useCase}</li>
+    ));
+    return <ul>{useCaseList}</ul>;
+  }
+
+  function dataModelDescription() {
+    const dataModelDescription = data.dataModelDescription.map(
+      (dataModelParagraph, idx) => (
+        <p
+          key={`dataModelDescription-${idx}`}
+          dangerouslySetInnerHTML={{ __html: dataModelParagraph }}
+        />
+      )
+    );
+    return <>{dataModelDescription}</>;
+  }
+
+  function Screenshots() {
+    const Screenshots = data.pluginScreenshots.map((screenshot, idx) => (
+      <>
+        {idx % 2 === 0 && (
+          <>
+            <Row key={`Screenshots-${idx}`} className="align-items-center">
+              <Col md={4}>
+                <br />
+                <h3 className="integrationsBlueH3">{screenshot.header}</h3>
+                <p>{screenshot.description}</p>
+              </Col>
+              <Col md={8}>
+                <Image
+                  className="productScreenshots border"
+                  src={screenshot.imageSrc}
+                  alt={screenshot.imageAlt}
+                  width={screenshot.imageWidth}
+                  height={screenshot.imageHeight}
+                />
+              </Col>
+            </Row>
+            <br />
+          </>
+        )}
+        {idx % 2 === 1 && (
+          <>
+            <Row key={`Screenshots-${idx}`} className="align-items-center">
+              <Col md={4} className="order-1 order-md-2">
+                <br />
+                <h3 className="integrationsBlueH3">{screenshot.header}</h3>
+                <p>{screenshot.description}</p>
+              </Col>
+              <Col md={8} className="order-2 order-md-1">
+                <Image
+                  className="productScreenshots border"
+                  src={screenshot.imageSrc}
+                  alt={screenshot.imageAlt}
+                  width={screenshot.imageWidth}
+                  height={screenshot.imageHeight}
+                />
+              </Col>
+            </Row>
+            <br />
+          </>
+        )}
+      </>
+    ));
+    return <>{Screenshots}</>;
+  }
+
+  if (!data) return <p>no plugin</p>;
+
+  return (
+    <>
+      <IntegrationsHead
+        name={data.name}
+        primaryType={data.primaryType}
+        slug={data.slug}
+      />
+      <Container>
+        {data.primaryType === "destination" && (
+          <DestionationHeaderArrow data={data} />
+        )}
+        {data.primaryType === "source" && <SourceHeaderArrow data={data} />}
+        <br />
+      </Container>
+      <div
+        id={`${data.name} and Use Cases`}
+        className="bg-dark text-white"
+        style={{ paddingTop: 50, paddingBottom: 50 }}
+      >
+        <Container>
+          <Row>
+            <Col lg={6} md={6} xs={12}>
+              <h2>Grouparoo &amp; {data.name}</h2>
+              <p>{data.useCases.paragraphOne}</p>
+              <p>{data.useCases.paragraphTwo}</p>
+              <p></p>
+            </Col>
+            <Col lg={6} md={6} xs={12}>
+              <h2>Use Cases</h2>
+              {useCaseList()}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <br />
+      <br />
+      <IntegrationInstallation
+        displayName={data.name}
+        packageName={data.packageName}
+      />
+
+      <Container>
+        <Row>
+          <Col>
+            <h2>{data.dataModelTitle}</h2>
+            {dataModelDescription()}
+          </Col>
+        </Row>
+      </Container>
+
+      <br />
+
+      <Container>
+        {data.destinationProfile && (
+          <Row>
+            <Col>
+              <h2>How Grouparoo Connects with {data.name}</h2>
+              <br />
+              <IntegrationMappings
+                name={data.name}
+                slug={data.slug}
+                image={data.logo}
+                destinationProfile={data.destinationProfile}
+                destinationProperty={data.destinationProperty}
+                destinationGroup={data.destinationGroup}
+              />
+            </Col>
+          </Row>
+        )}
+        <br />
+        <br />
+        <Screenshots />
+      </Container>
+
+      <br />
+      <br />
+      <div
+        id={`Grouparoo Connects with ${data.name}`}
+        className="bg-dark text-white"
+        style={{ paddingTop: 50, paddingBottom: 50 }}
+      >
+        <Container>
+          <Row>
+            <Col style={{ textAlign: "center" }}>
+              <h2>{data.otherPluginsHeading}</h2>
+              <br />
+              {data.primaryType === "destination" && <IntegrationSources />}
+              {data.primaryType === "source" && <IntegrationDestinations />}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <Container>
+        <GetStarted showLearnMore={false} />
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps(ctx) {
+  const { plugin, type } = ctx.params;
+  return { props: { plugin, type } };
+}
+
+export async function getStaticPaths() {
+  const paths = PluginData.map(
+    (p) => `/integrations/${p.primaryType}s/${p.slug}`
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+function DestionationHeaderArrow({ data }) {
+  return (
+    <>
+      <Row>
+        <Col lg={5} md={12}>
+          <br />
+          <h1>Sync {data.name} with all of your tools and customer data</h1>
+          <p>{data.description}</p>
+        </Col>
+        <Col lg={3} md={5} sm={5} xs={5} style={{ textAlign: "center" }}>
+          <br />
+          <IntegrationCard
+            name="Grouparoo"
+            slug="grouparoo"
+            image="grouparoo.png"
+            showLink={false}
+          />
+        </Col>
+        <Col lg={1} md={2} sm={2} xs={2} style={{ textAlign: "center" }}>
+          <br />
+          <br />
+          <br />
+          <BigArrow />
+        </Col>
+        <Col lg={3} md={5} sm={5} xs={5} style={{ textAlign: "center" }}>
+          <br />
+          <IntegrationCard
+            name={data.name}
+            slug={data.slug}
+            image={data.logo}
+            showLink={false}
+          />
+        </Col>
+      </Row>
+
+      <br />
+
+      <Row>
+        <Col />
+        <Col lg={3} md={4} sm={6} xs={6} style={{ textAlign: "center" }}></Col>
+        <Col />
+      </Row>
+    </>
+  );
+}
+
+function SourceHeaderArrow({ data }) {
+  return (
+    <>
+      <Row>
+        <Col lg={5} md={12}>
+          <br />
+          <h1>
+            Sync {data.name} customer data with all of your 3rd party tools
+          </h1>
+          <p>{data.description}</p>
+        </Col>
+        <Col lg={3} md={5} sm={5} xs={5} style={{ textAlign: "center" }}>
+          <br />
+          <IntegrationCard
+            name={data.name}
+            slug={data.slug}
+            image={data.logo}
+            showLink={false}
+          />
+        </Col>
+        <Col lg={1} md={2} sm={2} xs={2} style={{ textAlign: "center" }}>
+          <br />
+          <br />
+          <br />
+          <BigArrow />
+        </Col>
+        <Col lg={3} md={5} sm={5} xs={5} style={{ textAlign: "center" }}>
+          <br />
+          <IntegrationCard
+            name="Grouparoo"
+            slug="grouparoo"
+            image="grouparoo.png"
+            showLink={false}
+          />
+        </Col>
+      </Row>
+      <br />
+      <Row>
+        <Col />
+        <Col lg={3} md={4} sm={6} xs={6} style={{ textAlign: "center" }}></Col>
+        <Col />
+      </Row>
+    </>
+  );
+}
