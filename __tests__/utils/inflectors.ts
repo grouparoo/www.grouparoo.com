1d40678050f2f8cb180f7cac99c94c2c14332a4a
Teal Larson <teal.larson@grouparoo.com>
2021-04-21 14:46:45 -0700

    comparison page frontmatter now arrays of strings

--- __tests__/utils/inflectors.ts
+++ __tests__/utils/inflectors.ts
@@ -1,6 +1,6 @@
-import { titleize } from "../../utils/inflectors";
+import { titleize, arrayToReadableList, possessiveNoun } from "../../utils/inflectors";
 
-describe("titleize", () => {
+describe("#titleize", () => {
   test("capitalizes the first letter of every word", () => {
     const res = titleize("hello world");
     expect(res).toBe("Hello World");
@@ -14,3 +14,31 @@ describe("titleize", () => {
     expect(res).toBe("Hello-World");
   });
 });
+
+describe('#arrayToReadableList', () => {
+  test("throws error for empty array", () => {
+    expect(() => arrayToReadableList([])).toThrow(/string array empty/)
+  })
+  test("parses single item array correctly", () => {
+    expect(arrayToReadableList(["item1"])).toEqual("item1");
+  })
+  test("parses two item array correctly", () => {
+    expect(arrayToReadableList(["item1", "item2"])).toEqual("item1 and item2");
+  })
+  test("parses 3+ item array correctly", () => {
+    expect(arrayToReadableList(["item1", "item2", "item3"])).toEqual("item1, item2, and item3");
+
+  })
+})
+
+describe('#possessiveNoun', () => {
+  test("returns empty string if given empty string", () => {
+    expect(possessiveNoun('')).toEqual('');
+  })
+  test("returns a word ending in 's' with an ending apostrophe", () => {
+    expect(possessiveNoun("Grouparoo")).toEqual("Grouparoo's")
+  })
+  test("returns a word ending in anything except 's' with an ending '\'s'", () => {
+    expect(possessiveNoun("Ellis")).toEqual("Ellis'");
+  })
+})
--- utils/comparisonPages.ts
+++ utils/comparisonPages.ts
@@ -4,29 +4,21 @@ const component = {};
 
 export interface ComparisonInfo {
     competitor: string;
-    pro1: string;
-    pro2: string;
-    pro3: string;
-    pro4: string;
-    comp1: string;
-    comp2: string;
+    pros: string[];
+    competitorPros: string[];
     source: any;
 }
 
 
 export async function getComparison(slugId): Promise<ComparisonInfo> {
     const { source, frontMatter, path, slug } = await loadMdxFile(["pages", "solutions", `${slugId}.mdx`], component);
-
-    const { competitor, pro1, pro2, pro3, pro4, comp1, comp2 } = frontMatter;
+    console.log(frontMatter);
+    const { competitor, pros, competitorPros } = frontMatter;
 
     return {
         competitor,
-        pro1,
-        pro2,
-        pro3,
-        pro4,
-        comp1,
-        comp2,
+        pros,
+        competitorPros,
         source
     }
 }
