db48dd6c6f6ab329601ae9f7c08a4ecc6841b72f
Evan Tahler <evan.tahler@grouparoo.com>
2021-05-13 17:46:39 -0700

    disable posthog unless on www.grouparoo.com

--- components/posthog.tsx
+++ components/posthog.tsx
@@ -6,6 +6,8 @@ const apiKey = "oOnrFJPFSh7Ykn8p5u2VakV77rotgc7vKwoinolc4WA";
 
 export default function Posthog() {
   if (!isBrowser()) return null;
+  if (window?.location?.hostname !== "www.grouparoo.com") return null;
+
   posthog.init(apiKey, { api_host: posthogHost });
   return null;
 }
