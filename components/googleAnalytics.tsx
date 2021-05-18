ad0d6facdbe93fbac6316cada1b57b3449a3da76
Evan Tahler <evan.tahler@grouparoo.com>
2021-05-18 15:14:08 -0700

    Remove double-count for GA on next router change

--- components/googleAnalytics.tsx
+++ components/googleAnalytics.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import Head from "next/head";
-import Router from "next/router";
 
 const GoogleAnalyticsTrackingID = process.env.GOOGLE_ANALYTICS_TRACKING_ID;
 
@@ -18,14 +17,7 @@ gtag('config', '${GoogleAnalyticsTrackingID}');
     const script = document.createElement("script");
     script.innerHTML = scriptContent;
     document.body.appendChild(script);
-
-    Router.events.on("routeChangeComplete", () => pageView());
   }, []);
-
-  function pageView() {
-    // @ts-ignore
-    gtag("event", "page_view", { send_to: GoogleAnalyticsTrackingID });
-  }
 
   if (!GoogleAnalyticsTrackingID) return null;
 
