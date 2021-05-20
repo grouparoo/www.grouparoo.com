8a6dada98dd0fad1983080a6d02403901b86701c
Teal Larson <teal.larson@grouparoo.com>
2021-05-20 14:09:35 -0700

    pardot, mongodb, sqlite added to integrations page

---
+++ data/plugins/mongodb.ts
@@ -0,0 +1,19 @@
+import { Plugin } from "./index";
+
+export const MongoDB: Plugin = {
+  name: "MongoDB",
+  description: "",
+  priority: 40,
+  slug: "mongodb",
+  logo: "mongodb.svg",
+  primaryType: "source",
+  otherTypes: [""],
+  category: "database",
+  badge: "",
+  showLink: false,
+  packageName: "@grouparoo/mongodb",
+  useCases: {
+    paragraphOne: "",
+    paragraphTwo: "",
+  },
+};
