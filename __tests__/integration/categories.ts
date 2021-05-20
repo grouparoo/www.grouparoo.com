3f608b0eaafb5d078b75c2e7c412b465436715bf
Teal Larson <teal.larson@grouparoo.com>
2021-05-20 15:21:39 -0700

    replaced tests using sleep() with untilLocated

--- __tests__/integration/categories.ts
+++ __tests__/integration/categories.ts
@@ -1,75 +1,62 @@
 /**
  * @jest-environment jest-environment-webdriver
- * 
- * 
+ *
+ *
  */
 
- import * as helper from "../helpers/specHelper";
- let url: string;
- 
- // stub the selenium injected variables
- declare var browser: any;
- declare var by: any;
+import * as helper from "../helpers/specHelper";
+let url: string;
 
- // comparison pages to test
- const cases = ["reverse-etl", "customer-data-platform"];
+// stub the selenium injected variables
+declare var browser: any;
+declare var by: any;
+declare var until: any;
 
- 
- describe("integration/comparisons", () => {
-   beforeAll(async () => {
-     const env = await helper.prepareForIntegrationTest();
-     url = env.url;
-   }, 1000 * 60);
- 
-   afterAll(async () => {
-     await helper.shutdown();
-   });
+// comparison pages to test
+const cases = ["reverse-etl", "customer-data-platform"];
 
+describe("integration/comparisons", () => {
+  beforeAll(async () => {
+    const env = await helper.prepareForIntegrationTest();
+    url = env.url;
+  }, 1000 * 60);
 
-    test.each(cases)("it renders the copy for %p", async (pageSlug) => {
-    
+  afterAll(async () => {
+    await helper.shutdown();
+  });
+
+  test.each(cases)("it renders the copy for %p", async (pageSlug) => {
+    const testUrl = url + `/solutions/${pageSlug}`;
+    await browser.get(testUrl);
+    await browser.wait(until.elementLocated(by.tagName("h2")));
+    const H2s = await browser.findElements(by.tagName("h2"));
+    const H2Texts = await Promise.all(H2s.map((element) => element.getText()));
+    expect(H2Texts).toContain(`Get Started with Grouparoo`);
+  });
+
+  test.each(cases)(
+    "it renders the features banner for %p",
+    async (pageSlug) => {
       const testUrl = url + `/solutions/${pageSlug}`;
       await browser.get(testUrl);
-      await sleep();
-      const H2s = await browser.findElements(by.tagName("h2"))
-      const H2Texts = await Promise.all(H2s.map(element => element.getText()));
-      expect(H2Texts).toContain(`Get Started with Grouparoo`)
- 
-    });
- 
-    test.each(cases)("it renders the features banner for %p", async (pageSlug) => {
- 
-     const testUrl = url + `/solutions/${pageSlug}`;
-     await browser.get(testUrl);
-     await sleep();
-     const header = await browser.findElement(by.tagName("h2")).getText();
-     expect(header).toContain("that meets you where you are");
-     const H3s = await browser.findElements(by.tagName("h3"))
-     const H3Texts = await Promise.all(H3s.map(element => element.getText()));
-     expect(H3Texts).toContain(`🛍 Market more meaningfully`)
- 
-   });
- 
-   test.each(cases)("it renders section dividers for %p", async (pageSlug) => {
- 
-     const testUrl = url + `/solutions/${pageSlug}`;
-     await browser.get(testUrl);
-     await sleep();
-     const H3s = await browser.findElements(by.tagName("h3"))
-     const H3Texts = await Promise.all(H3s.map(element => element.getText()));
+      await browser.wait(until.elementLocated(by.tagName("h2")));
+      const header = await browser.findElement(by.tagName("h2")).getText();
+      expect(header).toContain("that meets you where you are");
+      const H3s = await browser.findElements(by.tagName("h3"));
+      const H3Texts = await Promise.all(
+        H3s.map((element) => element.getText())
+      );
+      expect(H3Texts).toContain(`🛍 Market more meaningfully`);
+    }
+  );
 
-     expect(H3Texts.length).toBeGreaterThan(0)
- 
-   });
-   
- });
- 
+  test.each(cases)("it renders section dividers for %p", async (pageSlug) => {
+    const testUrl = url + `/solutions/${pageSlug}`;
+    await browser.get(testUrl);
+    await browser.wait(until.elementLocated(by.tagName("h3")));
+    const H3s = await browser.findElements(by.tagName("h3"));
+    const H3Texts = await Promise.all(H3s.map((element) => element.getText()));
 
-async function sleep (time=1000) {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(null), time)
-  })
-}
-
-
-
+    expect(H3Texts.length).toBeGreaterThan(0);
+  });
+});
