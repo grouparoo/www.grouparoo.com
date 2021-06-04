23e31c592257c63dd57a39bc1daebfccb0ac89d9
Evan Tahler <evan.tahler@grouparoo.com>
2021-06-04 13:37:13 -0700

    Load app-examples list from Github

---
+++ components/docs/appExamplesReadme.tsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+import ReactMarkdown from "react-markdown";
+const readmeUrl =
+  "https://raw.githubusercontent.com/grouparoo/app-examples/main/README.md";
+
+export default function AppExamplesReadme() {
+  const [loading, setLoading] = useState(false);
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    load();
+  }, []);
+
+  async function load() {
+    setLoading(true);
+    const response = await fetch(readmeUrl);
+    const _content = await response.text();
+    setContent(_content);
+    setLoading(false);
+  }
+
+  if (loading) return <Spinner animation={"grow"} />;
+
+  return (
+    <p>
+      <ReactMarkdown>{content}</ReactMarkdown>
+    </p>
+  );
+}
