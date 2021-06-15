e8f03610e8ab745f0614e4af2656c5d9bfda6bf6
Andy Jih <andy.y.jih@gmail.com>
2021-06-15 14:17:15 -0700

    copy changes and card position swap

--- pages/get-started.tsx
+++ pages/get-started.tsx
@@ -30,11 +30,38 @@ export default function GetStarted({ props }) {
         <Container>
           <Row className="pt-5 pb-3">
             <Col>
-              <h1>Sync data how you want.</h1>
-              <h2>Lorem ipsum</h2>
+              <h1>Sync data how you want</h1>
+              <h2>
+                Use Grouparoo to power your Reverse ETL pipelines and workflows
+              </h2>
             </Col>
           </Row>
           <Row className="d-flex justify-content-center">
+            <Col className="mb-4 col-12 col-xl-5 col-md-7">
+              <Card
+                className="pricingCard mx-3 pt-3 pb-5 shadow-lg h-100 mb-4"
+                bg="white"
+              >
+                <Card.Header className="bg-white border-0">
+                  <Card.Title>
+                    <div className="h3 pt-2  mb-2">Self-Hosted</div>
+                    <hr className="col-9 mx-auto" />
+                  </Card.Title>
+                </Card.Header>
+                <Card.Body>
+                  Grouparoo is open source and free to run in your own cloud
+                </Card.Body>
+                <Card.Footer className="border-0 bg-white">
+                  <Button
+                    variant="outline-primary"
+                    href="/docs/getting-started"
+                    className="col-4"
+                  >
+                    Learn more
+                  </Button>
+                </Card.Footer>
+              </Card>
+            </Col>
             <Col className="mb-4 col-12 col-xl-5 col-md-7">
               <Card
                 className="pricingCard mx-3 pt-3 pb-4 shadow-lg h-100 mb-4"
@@ -57,32 +84,6 @@ export default function GetStarted({ props }) {
                 <small>No credit card required</small>
               </Card>
               <Link href="/pricing">View pricing plans</Link>
-            </Col>
-
-            <Col className="mb-4 col-12 col-xl-5 col-md-7">
-              <Card
-                className="pricingCard mx-3 pt-3 pb-5 shadow-lg h-100 mb-4"
-                bg="white"
-              >
-                <Card.Header className="bg-white border-0">
-                  <Card.Title>
-                    <div className="h3 pt-2  mb-2">Self-Hosted</div>
-                    <hr className="col-9 mx-auto" />
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body>
-                  Grouparoo is open source and free to run in your own cloud
-                </Card.Body>
-                <Card.Footer className="border-0 bg-white">
-                  <Button
-                    variant="outline-primary"
-                    href="/docs/getting-started"
-                    className="col-4"
-                  >
-                    Try it now
-                  </Button>
-                </Card.Footer>
-              </Card>
             </Col>
           </Row>
         </Container>
