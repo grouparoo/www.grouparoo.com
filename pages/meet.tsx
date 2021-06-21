a2e6482d73a3b868a5d9d3707411bf0f5e54e240
Teal Larson <teal.larson@grouparoo.com>
2021-06-21 10:00:40 -0700

    error on `/meet` form if no email (#532)
    * error on /meet if no email
    
    * disable browser-level validation

--- pages/meet.tsx
+++ pages/meet.tsx
@@ -13,13 +13,18 @@ interface FormError {
   generic: string;
 }
 export default function Meet() {
-  const { handleSubmit, register } = useForm();
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
   const { query } = useRouter();
   const [loading, setLoading] = useState(false);
   const [requested, setRequested] = useState(false);
   const [error, setError] = useState<FormError>({ email: null, generic: null });
 
   const errorHandler = new ErrorHandler();
+
   errorHandler.subscribe("result", (e) => {
     const error: FormError = { email: null, generic: null };
     const message = e?.error?.message || e?.message || e.toString();
@@ -142,7 +147,7 @@ export default function Meet() {
             <Error message={error.generic} />
             <Container className="bg-light p-4 mt-4 mt-md-0">
               <h3 className="mx-auto text-center">Schedule a 30 minute demo</h3>
-              <Form id="form" onSubmit={handleSubmit(onSubmit)}>
+              <Form id="form" onSubmit={handleSubmit(onSubmit)} noValidate>
                 {!requested ? (
                   <>
                     {" "}
@@ -176,6 +181,11 @@ export default function Meet() {
                         disabled={disabled}
                       />
                       <Error message={error.email} />
+                      {errors.email && (
+                        <small style={{ color: "red" }}>
+                          A valid email is required.
+                        </small>
+                      )}
                     </Form.Group>
                     <Form.Group>
                       <Form.Label>Company Name</Form.Label>
