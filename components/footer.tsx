ba63f531fa012879dc406d66bc487eaf4d7799f1
Teal Larson <teal.larson@grouparoo.com>
2021-07-13 10:50:21 -0700

    footer mobile/tablet layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -93,159 +93,171 @@ export default function Footer() {
   return (
     <footer className="footer py-5">
       <Container>
-        <Row>
-          <Col md={3} className="d-none d-lg-block">
+        <Row className="d-flex justify-content-between">
+          <Col lg={3} className="d-none d-lg-block">
             <LogoAndSocialLinks router={router} />
           </Col>
-          <Col md={9}>
+          <Col lg={9}>
             <Row>
-              <Col>
-                <strong>Use Cases</strong>
-                <br />
-                <Link href="/solutions/engineers">
-                  <a>Grouparoo for Engineers</a>
-                </Link>
-                <br />
-                <Link href="/solutions/marketers">
-                  <a>Grouparoo for Marketers</a>
-                </Link>
-                <br />
-                <Link href="/solutions/healthcare">
-                  <a>Healthcare</a>
-                </Link>
-                <br />
-                <Link href="/solutions/education">
-                  <a>Education</a>
-                </Link>
-                <br />
-                <Link href="/solutions/segment-alternative">
-                  <a>Segment Alternative</a>
-                </Link>
-                <br />
-                <Link href="/solutions/census-alternative">
-                  <a>Census Alternative</a>
-                </Link>
-                <br />
-                <Link href="/solutions/hightouch-alternative">
-                  <a>Hightouch Alternative</a>
-                </Link>
-                <br />
+              <Col className="d-none d-md-inline-block col-md-1" />
 
-                <Link href="/solutions/reverse-etl">
-                  <a>Reverse ETL</a>
-                </Link>
-                <br />
+              <Col className="col-6 col-md-3 pb-2 px-3 ">
+                <div className="pl-1">
+                  <strong>Use Cases</strong>
+                  <br />
+                  <Link href="/solutions/engineers">
+                    <a>Grouparoo for Engineers</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/marketers">
+                    <a>Grouparoo for Marketers</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/healthcare">
+                    <a>Healthcare</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/education">
+                    <a>Education</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/segment-alternative">
+                    <a>Segment Alternative</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/census-alternative">
+                    <a>Census Alternative</a>
+                  </Link>
+                  <br />
+                  <Link href="/solutions/hightouch-alternative">
+                    <a>Hightouch Alternative</a>
+                  </Link>
+                  <br />
 
-                <Link href="/solutions/modern-data-stack">
-                  <a>Modern Data Stack</a>
-                </Link>
+                  <Link href="/solutions/reverse-etl">
+                    <a>Reverse ETL</a>
+                  </Link>
+                  <br />
+
+                  <Link href="/solutions/modern-data-stack">
+                    <a>Modern Data Stack</a>
+                  </Link>
+                </div>
+
+                <hr className="d-md-none mx-auto border-light border-top-0 m-2" />
               </Col>
-              <Col>
-                <strong>Integrations</strong>
-                <br />
-                <Link href="/integrations/destinations/salesforce">
-                  <a>Salesforce Data Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/destinations/zendesk">
-                  <a>Zendesk Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/destinations/marketo">
-                  <a>Marketo Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/destinations/mailchimp">
-                  <a>Mailchimp Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/destinations/hubspot">
-                  <a>Hubspot Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/destinations/iterable">
-                  <a>Iterable Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/sources/bigquery">
-                  <a>Bigquery Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/sources/snowflake">
-                  <a>Snowflake Integration</a>
-                </Link>
-                <br />
-                <Link href="/integrations/sources/redshift">
-                  <a>Snowflake Integration</a>
-                </Link>
-                <br />
+              <Col className="col-6 col-md-3 pb-2 px-3">
+                <div className="pl-1">
+                  <strong>Integrations</strong>
+                  <br />
+                  <Link href="/integrations/destinations/salesforce">
+                    <a>Salesforce Data Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/destinations/zendesk">
+                    <a>Zendesk Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/destinations/marketo">
+                    <a>Marketo Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/destinations/mailchimp">
+                    <a>Mailchimp Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/destinations/hubspot">
+                    <a>Hubspot Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/destinations/iterable">
+                    <a>Iterable Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/sources/bigquery">
+                    <a>Bigquery Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/sources/snowflake">
+                    <a>Snowflake Integration</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations/sources/redshift">
+                    <a>Snowflake Integration</a>
+                  </Link>
+                  <br />
+                </div>
+                <hr className="d-md-none mx-auto border-light border-top-0 m-2" />
               </Col>
-              <Col>
-                <strong>Product</strong>
-                <br />
-                <Link href="/docs">
-                  <a>Docs</a>
-                </Link>
-                <br />
-                <Link href="/pricing">
-                  <a>Pricing</a>
-                </Link>
-                <br />
-                <Link href="/integrations">
-                  <a>Integrations</a>
-                </Link>
-                <br />
-                <Link href="/whats-new">
-                  <a>What's New</a>
-                </Link>
-                <br />
-                <Link href="/docs/community">
-                  <a>Community</a>
-                </Link>
-                <br />
-                <a
-                  target="_blank"
-                  href="https://github.com/grouparoo/grouparoo"
-                  rel="noreferrer"
-                >
-                  Github
-                </a>
+              <Col className="col-6 col-md-2 pb-2 px-3">
+                <div className="pl-1">
+                  <strong>Product</strong>
+                  <br />
+                  <Link href="/docs">
+                    <a>Docs</a>
+                  </Link>
+                  <br />
+                  <Link href="/pricing">
+                    <a>Pricing</a>
+                  </Link>
+                  <br />
+                  <Link href="/integrations">
+                    <a>Integrations</a>
+                  </Link>
+                  <br />
+                  <Link href="/whats-new">
+                    <a>What's New</a>
+                  </Link>
+                  <br />
+                  <Link href="/docs/community">
+                    <a>Community</a>
+                  </Link>
+                  <br />
+                  <a
+                    target="_blank"
+                    href="https://github.com/grouparoo/grouparoo"
+                    rel="noreferrer"
+                  >
+                    Github
+                  </a>
+                </div>
               </Col>
-              <Col>
-                <strong>Company</strong>
-                <br />
-                <Link href="/about">
-                  <a>About Us</a>
-                </Link>
-                <br />
-                <Link href="/blog">
-                  <a>Blog</a>
-                </Link>
-                <br />
-                <Link href="/docs/support">
-                  <a>Support</a>
-                </Link>
-                <br />
-                <Link href="/meet">
-                  <a>Contact Us</a>
-                </Link>
-                <br />
-                <Link href="/about#careers">
-                  <a>Careers</a>
-                </Link>
-                <br />
-                <Link href="/legal/privacy">
-                  <a>Privacy Policy</a>
-                </Link>
+              <Col className="col-6 col-md-2 pb-2 px-3">
+                <div className="pl-1">
+                  <strong>Company</strong>
+                  <br />
+                  <Link href="/about">
+                    <a>About Us</a>
+                  </Link>
+                  <br />
+                  <Link href="/blog">
+                    <a>Blog</a>
+                  </Link>
+                  <br />
+                  <Link href="/docs/support">
+                    <a>Support</a>
+                  </Link>
+                  <br />
+                  <Link href="/meet">
+                    <a>Contact Us</a>
+                  </Link>
+                  <br />
+                  <Link href="/about#careers">
+                    <a>Careers</a>
+                  </Link>
+                  <br />
+                  <Link href="/legal/privacy">
+                    <a>Privacy Policy</a>
+                  </Link>
+                </div>
               </Col>
+              <Col className="d-none d-md-inline-block col-md-1" />
             </Row>
           </Col>
         </Row>
 
         <Row className="d-lg-none">
-          <Col
-            style={{ textAlign: "center", paddingTop: 50, paddingBottom: 50 }}
-          >
+          <Col className="text-center pb-4 pt-5">
             <LogoAndSocialLinks router={router} />
           </Col>
         </Row>
