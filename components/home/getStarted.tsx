4e84cc16800141198a346bb25923bd44398eadc0
Evan Tahler <evan.tahler@grouparoo.com>
2021-07-20 10:51:13 -0700

    remove un-used container

--- components/home/getStarted.tsx
+++ components/home/getStarted.tsx
@@ -7,45 +7,43 @@ export default function GetStarted({
   showLearnMore?: boolean;
 }) {
   return (
-    <>
-      <div id="getStartedContainer" className="text-center w-full pb-5">
-        <Container>
-          <Row>
-            <Col md={3} />
-            <Col>
-              <br />
-              <br />
-              <Card border="primary">
-                <Card.Body>
-                  <h2>Get Started with Grouparoo</h2>
-                  <p>Start syncing your data with Grouparoo Cloud</p>
-                  <Button
-                    variant="primary"
-                    href="/trial"
-                    className="ml-auto mt-1 mb-4"
-                  >
-                    Start Free Trial
-                  </Button>
-                  {showLearnMore ? (
-                    <>
-                      <p>
-                        <small>
-                          Or download and try our open source{" "}
-                          <Link href="/docs/getting-started">
-                            <a> Community edition</a>
-                          </Link>
-                          .
-                        </small>
-                      </p>
-                    </>
-                  ) : null}
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={3} />
-          </Row>
-        </Container>
-      </div>
-    </>
+    <div id="getStartedContainer" className="text-center w-full pb-5">
+      <Container>
+        <Row>
+          <Col md={3} />
+          <Col>
+            <br />
+            <br />
+            <Card border="primary">
+              <Card.Body>
+                <h2>Get Started with Grouparoo</h2>
+                <p>Start syncing your data with Grouparoo Cloud</p>
+                <Button
+                  variant="primary"
+                  href="/trial"
+                  className="ml-auto mt-1 mb-4"
+                >
+                  Start Free Trial
+                </Button>
+                {showLearnMore ? (
+                  <>
+                    <p>
+                      <small>
+                        Or download and try our open source{" "}
+                        <Link href="/docs/getting-started">
+                          <a> Community edition</a>
+                        </Link>
+                        .
+                      </small>
+                    </p>
+                  </>
+                ) : null}
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={3} />
+        </Row>
+      </Container>
+    </div>
   );
 }
