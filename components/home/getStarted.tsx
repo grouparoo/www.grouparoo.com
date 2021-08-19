1443f8f3cf5e4b0ba4e63c18cd7d339dacc0076a
Evan Tahler <evan.tahler@grouparoo.com>
2021-08-19 09:47:30 -0700

    Got the component on the page + refactor

--- components/home/getStarted.tsx
+++ components/home/getStarted.tsx
@@ -1,4 +1,4 @@
-import { Container, Row, Col, Button, Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import Link from "next/link";
 
 export default function GetStarted({
@@ -7,43 +7,25 @@ export default function GetStarted({
   showLearnMore?: boolean;
 }) {
   return (
-    <div id="getStartedContainer" className="text-center w-full pb-5">
-      <Container>
-        <Row>
-          <Col md={3} />
-          <Col>
-            <br />
-            <br />
-            <Card border="primary">
-              <Card.Body>
-                <h2>Get Started with Grouparoo</h2>
-                <p>Start syncing your data with Grouparoo Cloud</p>
-                <Button
-                  variant="primary"
-                  href="/trial"
-                  className="ml-auto mt-1 mb-4"
-                >
-                  Start Free Trial
-                </Button>
-                {showLearnMore ? (
-                  <>
-                    <p>
-                      <small>
-                        Or download and try our open source{" "}
-                        <Link href="/docs/getting-started">
-                          <a> Community edition</a>
-                        </Link>
-                        .
-                      </small>
-                    </p>
-                  </>
-                ) : null}
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col md={3} />
-        </Row>
-      </Container>
-    </div>
+    <Card border="primary">
+      <Card.Body>
+        <h2>Get Started with Grouparoo</h2>
+        <p>Start syncing your data with Grouparoo Cloud</p>
+        <Button variant="primary" href="/trial" className="ml-auto mt-1 mb-4">
+          Start Free Trial
+        </Button>
+        {showLearnMore ? (
+          <p>
+            <small>
+              Or download and try our open source{" "}
+              <Link href="/docs/getting-started">
+                <a> Community edition</a>
+              </Link>
+              .
+            </small>
+          </p>
+        ) : null}
+      </Card.Body>
+    </Card>
   );
 }
