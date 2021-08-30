adeb817c662a5796e853a3f60ba47d714f584dab
Teal Larson <teal.larson@grouparoo.com>
2021-08-30 13:42:11 -0700

    add plugin file for calculate-property

---
+++ data/plugins/calculated-property.ts
@@ -0,0 +1,16 @@
+import { Plugin } from "./index";
+
+export const CalculatedProperty: Plugin = {
+  name: "Calculated Property",
+  description:
+    "Grouparoo's Calculated Property plugin allows you to transform data from your warehouse before you export to various destinations such as CRMs, Customer Support Tools, and Marketing Tools.",
+  priority: 0,
+  slug: "calculated-property",
+  logo: "calculated-property.png",
+  primaryType: "source",
+  otherTypes: [],
+  category: "transform tool",
+  badge: "",
+  showLink: false,
+  packageName: "@grouparoo/calculated-property",
+};
