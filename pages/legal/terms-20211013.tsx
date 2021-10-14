fd653ec953546e63834fe3cdd6cca968de89d658
Andy Jih <andy.y.jih@gmail.com>
2021-10-13 19:08:16 -0700

    timestamped terms that we can directly link to

---
+++ pages/legal/terms-20211013.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import { Container, Row, Col } from "react-bootstrap";
+import { loadMdxFile } from "../../utils/mdxUtils";
+import hydrate from "next-mdx-remote/hydrate";
+import { PageNavigation } from "../../components/pageNavigation";
+
+export default function Meet({ pageProps }) {
+  const content = hydrate(pageProps.source, {});
+
+  return (
+    <>
+      <Head>
+        <title>Grouparoo Master Terms and Conditions</title>
+        <link
+          rel="canonical"
+          href="https://www.grouparoo.com/legal/terms-20211013"
+        />
+      </Head>
+
+      <Container className="mb-5">
+        <Row>
+          <Col md={9}>{content}</Col>
+          <Col md={3}>
+            <div
+              style={{
+                position: "sticky",
+                top: 10,
+              }}
+            >
+              <PageNavigation />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const props = await loadMdxFile(["data", "legal", "terms-20211013.md"], {});
+  return { props };
+}
