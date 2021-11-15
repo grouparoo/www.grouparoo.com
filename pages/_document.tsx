6d7a14f40f86a1115bb5dc49abcb8baf8c90a2c7
Evan Tahler <evan.tahler@grouparoo.com>
2021-11-15 11:26:04 -0800

    Load missing font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
 
           {/* Google Tag Manager + Google Analytics */}
           <script
