dd1f05b4576b4d79970e23d7c83d8df81e36fdbc
Evan Tahler <evan.tahler@grouparoo.com>
2021-11-15 11:56:54 -0800

    Fix fonts (#782)
    * remove duplicate font entry
    
    * try preconnect
    
    * use font-inlining
    
    * Load missing font
    
    * build font URL progamarically

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
+const fontFamilies = [
+  "IBM+Plex+Mono:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700",
+  "IBM+Plex+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;1,300;1,400",
+  "Inter:wght@100;300;400;500;700;900",
+];
+const fontUrl = `https://fonts.googleapis.com/css2?${fontFamilies
+  .map((f) => `family=${f}`)
+  .join("&")}&display=swap`;
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
+          {/* Google Fonts */}
+          <link href={fontUrl} rel="stylesheet" />
+
+          {/* Google Tag Manager + Google Analytics */}
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_TRACKING_ID}`}
@@ -35,6 +48,7 @@ class MyDocument extends Document {
             }}
           />
         </Head>
+
         <body>
           <Main />
           <NextScript />
