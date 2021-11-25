78f5c089e2d641e09b083050d245adfd057a9da0
Brian Leonard <brian@bleonard.com>
2021-11-24 16:53:22 -0800

    Salesforce has multiple integrations

--- data/plugins/salesforce.ts
+++ data/plugins/salesforce.ts
@@ -3,7 +3,7 @@ import { Plugin } from "./index";
 export const Salesforce: Plugin = {
   name: "Salesforce",
   description:
-    "Grouparoo's Salesforce data integration allows you to sync all of your customer data that lives in your data warehouse into Salesforce.",
+    "Grouparoo's Salesforce data integration allows you to sync all of your  data that lives in your data warehouse into Salesforce.",
   priority: 100,
   slug: "salesforce",
   logo: "salesforce.png",
@@ -15,28 +15,40 @@ export const Salesforce: Plugin = {
   packageName: "@grouparoo/salesforce",
   useCases: {
     paragraphOne:
-      "Grouparoo is open source data infrastructure to keep customer data in-sync between your data warehouse and 3rd party tools such as Salesforce.",
+      "Grouparoo is open source data infrastructure to keep data in-sync between your data warehouse and 3rd party tools such as Salesforce.",
     paragraphTwo:
-      "Grouparoo pulls customer data in real-time from all of your data sources and warehouse and keeps that data synced to Salesforce.",
+      "Grouparoo pulls data in real-time from all of your data sources and warehouse and keeps that data synced to Salesforce.",
   },
   useCasesList: [
-    "Create and update any recordObject in Salesforce such as Contacts, Leads, or Individuals as new users sign up in your core product",
-    "Update existing Fields on people objects such as email and first name and also create new custom Fields on your own terms without needing engineering help",
-    "Automatically create Campaigns or add people to Campaigns in Salesforce",
-    "Keep Lead Fields up-to-date automatically",
-    "Have better control over how you segment your Leads and Contacts",
+    "Keep Contact, Lead, Account, or any Custom Object Property values up-to-date with your data warehouse",
+    "Automatically create Contacts in Salesforce as new users sign up in your core product",
+    "Update Companies with data to better drive Sales motions",
+    "Trigger email Campaigns based on segmentation",
   ],
   dataModelTitle: "An Overview of Salesforce's Data Model and API",
   dataModelDescription: [
-    "Trying to figure out how to integrate your data with Salesforce? Here’s an overview for how Salesforce defines customer objects.",
-    "At its core, Salesforce is a database, and a very flexible one at that. While most of the objects we name here are the primary default ways to use Salesforce, you can map Grouparoo records to just about any kind of object in Salesforce.",
-    "Salesforce has a few primary objects: Leads, Contacts, and Individuals. Each of those three previous objects represents an individual person who might be a lead or a customer. These objects all have two required fields: Email and LastName",
+    "Trying to figure out how to integrate your data with Salesforce? Here’s an overview for how Salesforce works with Grouparoo.",
+    "At its core, Salesforce is a database, and a very flexible one at that. While most of the Objects we name here are the primary default ways to use Salesforce, you can map Grouparoo records to just about any kind of Object in Salesforce.",
+    "Salesforce has a few primary objects: Leads, Contacts, and Accounts. Each of these represents an entity that you can sync with your data warehouse. Grouparoo is not limited to these Objects, though. Any Object can be synced. Each Object has their own required fields.",
   ],
   destinationMappings: [
     {
-      record: "Salesforce Lead/Contact",
-      property: "Salesforce Lead/Contact Fields",
+      record: "Salesforce Contacts",
+      property: "Salesforce Contact Fields",
       group: "Salesforce Campaigns",
+    },
+    {
+      record: "Salesforce Accounts",
+      property: "Salesforce Account Fields",
+    },
+    {
+      record: "Salesforce Leads",
+      property: "Salesforce Leads Fields",
+      group: "Salesforce Campaigns",
+    },
+    {
+      record: "Salesforce Custom Objects",
+      property: "Salesforce Custom Object Fields",
     },
   ],
   pluginScreenshots: [
@@ -61,10 +73,9 @@ export const Salesforce: Plugin = {
       imageHeight: 506,
     },
   ],
-  otherPluginsHeading:
-    "Import your customer data from these services into Salesforce",
+  otherPluginsHeading: "Import your data from these services into Salesforce",
   narrative: {
-    p1: "Connect your data warehouse with Salesforce using Grouparoo.  Grouparoo enables you to read data from multiple resources and sync it straight to Salesforce for your marketing team to access. No overbuilt systems or spending time and engineering resources to build your own data management pipeline.  Our application features over 20 integration plugins including our Google BigQuery integration and our Postgres integration, so you can connect all your external applications to gather a singular view of your data -- even if your tools change over time.",
-    p2: "Our user experience includes both UI and  CLI options, making it the best option for your entire team -- whether they’re using it to drive products, content, or marketing.  Having a single view of your data in a data warehouse is powerful, but connecting it with your CRM will enable you to do your best work. Security is no issue -- Grouparoo can be either self-hosted, or used as a Software as a Service tool. With Grouparoo, you can rest knowing your data integration solution is helping lead the Reverse ETL space.  Even if you’re still early on in your search for a data pipeline solution, check out our documentation to view all that Grouparoo can do, or get started with a Free Trial of our hosting offering today!",
+    p1: "Connect your data warehouse with Salesforce using Grouparoo.  Grouparoo enables you to read data from multiple resources and sync it straight to Salesforce for your Sales team to access. No overbuilt systems or spending time and engineering resources to build your own data management pipeline.  Our application features over 20 integration plugins including our Google BigQuery integration and our Postgres integration, so you can connect all your external applications to gather a singular view of your data -- even if your tools change over time.",
+    p2: "Our user experience includes both UI and CLI options, making it the best option for your entire team -- whether they’re using it to drive sales, products, content, or marketing.  Having a single view of your data in a data warehouse is powerful, but connecting it with your CRM will enable you to do your best work. Security is no issue -- Grouparoo can be either self-hosted, or used as a Software as a Service tool. With Grouparoo, you can rest knowing your data integration solution is helping lead the Reverse ETL space.  Even if you’re still early on in your search for a data pipeline solution, check out our documentation to view all that Grouparoo can do, or get started with a free trial of our hosting offering today!",
   },
 };
