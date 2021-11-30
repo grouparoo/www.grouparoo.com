7cbc0c8779bc84e9be408aec14dc76291f0a092d
Krishna Glick <krishna.glick@grouparoo.com>
2021-11-30 16:17:25 -0500

    unified customer records

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,6 +18,7 @@ import { About } from "../components/home/about/About";
 import { Actionable } from "../components/home/actionable/Actionable";
 import { Testimonials } from "../components/home/testimonials/Testimonials";
 import { HowGrouparooWorks } from "../components/home/how-it-works/HowGrouparooWorks";
+import styles from "./index.module.scss";
 
 const getTagline = (pluginNames: string[]) =>
   `Stop writing code to sync data to ${
@@ -83,6 +84,38 @@ const IndexPage = ({ pageProps, setReleaseNote }) => {
       <Actionable />
       <Testimonials />
       <HowGrouparooWorks />
+      <Container className={styles.container}>
+        <Row>
+          <Col>
+            <img src="/images/home/how-it-works-1.png" />
+          </Col>
+          <Col>
+            <section>
+              <h3>Create Unified Customer Records</h3>
+              <p>
+                Create a single view of your customers from all of your data
+                sources. Define what data you care about either with Code or in
+                our Enterprise UI.
+              </p>
+            </section>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <section>
+              <h3>Create Unified Customer Records</h3>
+              <p>
+                Create a single view of your customers from all of your data
+                sources. Define what data you care about either with Code or in
+                our Enterprise UI.
+              </p>
+            </section>
+          </Col>
+          <Col>
+            <img src="/images/home/how-it-works-2.png" />
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
