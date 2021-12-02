a31abd4d7f97800ddb3d99586d7e7e6f29e79ab9
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-02 09:20:07 -0500

    Features started

---
+++ components/home/features/Features.tsx
@@ -0,0 +1,17 @@
+import { Col, Container, Row } from "react-bootstrap";
+
+export const Features = () => {
+  return (
+    <Container>
+      <Row>
+        <h2>Features that embed in your workflow</h2>
+        <p>Grouparoo makes it easy to follow developer best practices.</p>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
+};
