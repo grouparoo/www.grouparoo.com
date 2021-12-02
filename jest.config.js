a2938f8c0caa018457e2483fd30f865b29411b95
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-02 16:06:35 -0500

    tests run

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,6 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__tests__/helpers/mocks/fileMock.js",
-    "\\.(css|sass)$": "<rootDir>/__tests__/helpers/mocks/styleMock.js",
+    "\\.(css|sass|scss)$": "<rootDir>/__tests__/helpers/mocks/styleMock.js",
   },
 };
