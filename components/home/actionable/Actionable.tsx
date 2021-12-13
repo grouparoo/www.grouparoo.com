6c46b273c75e266b933696c3a77a9e1e1f372d01
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-13 13:29:40 -0500

    Actionable area improvements

--- components/home/actionable/Actionable.tsx
+++ components/home/actionable/Actionable.tsx
@@ -7,30 +7,36 @@ import styles from "./Actionable.module.scss";
 export const Actionable = () => {
   return (
     <Container fluid className={`weave ${styles.container}`}>
-      <Row>
-        <Col>
-          <RecordDisplay className={styles.recordDisplay} />
-        </Col>
-        <Col>
-          <section>
-            <h2>Reverse ETL makes your data warehouse actionable</h2>
-            <p>
-              Grouparoo connects your customer data to all the places where it
-              can make a difference. Use your data warehouse to power marketing
-              campaigns, customer service, and everything else your business
-              needs to succeed.
-            </p>
-            <Link href="/solutions/reverse-etl" passHref={true}>
-              <Button type="button" variant="primary" className="rounded-pill">
-                Learn More
-              </Button>
-            </Link>
-          </section>
-        </Col>
-      </Row>
-      <Row className={styles.bottomRow}>
-        <ActionableTargets />
-      </Row>
+      <Container>
+        <Row>
+          <Col>
+            <RecordDisplay className={styles.recordDisplay} />
+          </Col>
+          <Col>
+            <section>
+              <h2>Reverse ETL makes your data warehouse actionable</h2>
+              <p>
+                Grouparoo connects your customer data to all the places where it
+                can make a difference. Use your data warehouse to power
+                marketing campaigns, customer service, and everything else your
+                business needs to succeed.
+              </p>
+              <Link href="/solutions/reverse-etl" passHref={true}>
+                <Button
+                  type="button"
+                  variant="primary"
+                  className="rounded-pill"
+                >
+                  Learn More
+                </Button>
+              </Link>
+            </section>
+          </Col>
+        </Row>
+        <Row className={styles.bottomRow}>
+          <ActionableTargets />
+        </Row>
+      </Container>
     </Container>
   );
 };
