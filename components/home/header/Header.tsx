d1fbf1156c771e314efebc049eb3c93cc4e6d736
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-13 13:33:53 -0500

    Container improvements

--- components/home/header/Header.tsx
+++ components/home/header/Header.tsx
@@ -9,22 +9,24 @@ interface HeaderProps {
 
 export const Header = ({ tagline }: HeaderProps) => (
   <Container fluid className={`pipes ${styles.container}`} data-testid="header">
-    <Row>
-      <Col md="6" className={`${styles.text}`}>
-        <h2>{tagline}</h2>
-        <p className="outline">
-          Grouparoo is an open source framework that helps you move data between
-          your data warehouse and all of your cloud-based tools.
-        </p>
-        <Link href="/get-started" passHref>
-          <Button type="button" variant="primary" className="rounded-pill">
-            Get Started
-          </Button>
-        </Link>
-      </Col>
-      <Col md="6" className={`${styles.image}`}>
-        <HeaderImage className={styles.headerImage} />
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col md="6" className={`${styles.text}`}>
+          <h2>{tagline}</h2>
+          <p className="outline">
+            Grouparoo is an open source framework that helps you move data
+            between your data warehouse and all of your cloud-based tools.
+          </p>
+          <Link href="/get-started" passHref>
+            <Button type="button" variant="primary" className="rounded-pill">
+              Get Started
+            </Button>
+          </Link>
+        </Col>
+        <Col md="6" className={`${styles.image}`}>
+          <HeaderImage className={styles.headerImage} />
+        </Col>
+      </Row>
+    </Container>
   </Container>
 );
--- components/home/why-grouparoo/WhyGrouparoo.tsx
+++ components/home/why-grouparoo/WhyGrouparoo.tsx
@@ -8,49 +8,51 @@ import styles from "./WhyGrouparoo.module.scss";
 export const WhyGrouparoo = () => {
   return (
     <Container fluid className={`pipes ${styles.container}`}>
-      <Row>
-        <h2>Why Open Source?</h2>
-        <p className={styles.subheading}>
-          Keeping customer data secure is more important than ever
-        </p>
-      </Row>
-      <Row>
-        <Col>
-          <Github className={styles.icon} />
-          <h3>Control</h3>
-          <p>
-            Grouparoo runs in your environment so you always have access to all
-            the code and the data. Use it however you want.
+      <Container>
+        <Row>
+          <h2>Why Open Source?</h2>
+          <p className={styles.subheading}>
+            Keeping customer data secure is more important than ever
           </p>
-        </Col>
-        <Col>
-          <BriefcaseCheck className={styles.icon} />
-          <h3>Compliance</h3>
-          <p>
-            The control and privacy features make it easier for you to comply
-            with GDPR, CCPA, and industry-specific standards.
-          </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Mask className={styles.icon} />
-          <h3>Privacy</h3>
-          <p>
-            You get to choose exactly what data gets sent to each tool. And you
-            give them less because it was only needed to make the groups.
-          </p>
-        </Col>
-        <Col>
-          <MoneyBill className={styles.icon} />
-          <h3>Cost</h3>
-          <p>
-            Because Grouparoo is hosted in your private cloud, we’re
-            significantly cheaper than SaaS alternatives that charge based on
-            usage.
-          </p>
-        </Col>
-      </Row>
+        </Row>
+        <Row>
+          <Col>
+            <Github className={styles.icon} />
+            <h3>Control</h3>
+            <p>
+              Grouparoo runs in your environment so you always have access to
+              all the code and the data. Use it however you want.
+            </p>
+          </Col>
+          <Col>
+            <BriefcaseCheck className={styles.icon} />
+            <h3>Compliance</h3>
+            <p>
+              The control and privacy features make it easier for you to comply
+              with GDPR, CCPA, and industry-specific standards.
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Mask className={styles.icon} />
+            <h3>Privacy</h3>
+            <p>
+              You get to choose exactly what data gets sent to each tool. And
+              you give them less because it was only needed to make the groups.
+            </p>
+          </Col>
+          <Col>
+            <MoneyBill className={styles.icon} />
+            <h3>Cost</h3>
+            <p>
+              Because Grouparoo is hosted in your private cloud, we’re
+              significantly cheaper than SaaS alternatives that charge based on
+              usage.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
