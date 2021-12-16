c8b314d941253113b00adc85b3c0f6771ac03d4f
Krishna Glick <krishna.glick@grouparoo.com>
2021-12-16 12:19:33 -0500

    Spotify + contrast

--- components/home/partners/Partners.tsx
+++ components/home/partners/Partners.tsx
@@ -1,20 +1,24 @@
+import { Container } from "react-bootstrap";
 import { Bloq } from "./logos/Bloq";
 import { Chipax } from "./logos/Chipax";
 import { MindfulChef } from "./logos/MindfulChef";
 import { OneMedical } from "./logos/OneMedical";
 import { Raisedev } from "./logos/Raisedev";
+import { Spotify } from "./logos/Spotify";
 import styles from "./Partners.module.scss";
 
 export const Partners = () => (
-  <div
-    className={styles.partners + " d-flex flex-column justify-content-center"}
-  >
-    <div className="d-flex justify-content-around align-items-center">
-      <OneMedical />
-      <MindfulChef className={styles.mindfulChef} />
-      <Chipax />
-      <Raisedev />
-      <Bloq />
+  <Container>
+    <div
+      className={styles.partners + " d-flex flex-column justify-content-center"}
+    >
+      <div className="d-flex justify-content-between align-items-center">
+        <Spotify />
+        <OneMedical />
+        <MindfulChef className={styles.mindfulChef} />
+        <Raisedev />
+        <Bloq />
+      </div>
     </div>
-  </div>
+  </Container>
 );
---
+++ components/home/partners/logos/Spotify.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export const Spotify = () => (
+  <Image
+    src="/images/home/customer-logos/spotify.png"
+    width={242}
+    height={83}
+    alt={"Spotify R&D logo"}
+  />
+);
