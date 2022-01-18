1869cd7d1522a04fdb01f6c91ad80b46033d31fa
Edmundo Ruiz Ghanem <168664+edmundito@users.noreply.github.com>
2022-01-18 09:31:40 -0500

    Add Mailjet destination page, update logo, and what's new post (#871)
    * Add Mailjet destination page, whats new post, and update logo and screen shots
    
    * Fix copy in mailjet whatsnew post
    
    * Add Grouparoo to Mailjet concepts image to whatsnew post
    
    * Updating things automatically
    
    * Minor copy updates to Maijlet announcement and plugin page
    
    * Update mailjet whatsnew post to mention v0.6 instead of v0.4 (whoops)
    
    * Updating things automatically
    
    * Remove unnecessary "with"
    
    Co-authored-by: Pedro S. Lopez <pedro.lopez@grouparoo.com>
    
    * Updating things automatically
    
    Co-authored-by: Grouparoo Bot <hello@grouparoo.com>
    Co-authored-by: Pedro S. Lopez <pedro.lopez@grouparoo.com>

--- data/plugins/mailjet.ts
+++ data/plugins/mailjet.ts
@@ -11,6 +11,42 @@ export const Mailjet: Plugin = {
   otherTypes: [],
   category: "email marketing",
   badge: "",
-  showLink: false,
+  showLink: true,
   packageName: "@grouparoo/mailjet",
+  useCases: {
+    paragraphOne:
+      "Grouparoo is open source data infrastructure to keep customer data in-sync between your data warehouse and 3rd party tools such as Mailjet.",
+    paragraphTwo:
+      "Grouparoo pulls customer data in real-time from all of your data sources and warehouse and keeps that data synced to Mailjet.",
+  },
+  useCasesList: [
+    "Automatically create Contacts as new users sign up in your core product",
+    "Trigger email campaigns based on dynamic Audiences",
+  ],
+  dataModelTitle: "An Overview of Mailjet’s Data Model and API",
+  dataModelDescription: [
+    "Trying to figure out how to integrate with Mailjet? Here’s an overview for how Mailjet defines customers.",
+    "The core object in Mailjet is a Contact. A Contact represents an individual person that lives on a Contact list. Contacts have one required attribute, which is their email address.",
+    "Mailjet calls attributes of a Contact a Contact Property. Mailjet pre-defines a number of Contact Properties such as <code>firstname</code> and <code>country</code>, but you can define as many Contact Properties as you'd like in Mailjet.",
+  ],
+  destinationMappings: [
+    {
+      record: "Mailjet Contacts",
+      property: "Mailjet Contact Properties",
+      group: "Mailjet Lists",
+    },
+  ],
+  pluginScreenshots: [
+    {
+      header: "Define what customer data you want in Mailjet",
+      description:
+        "Grouparoo is low-code, so non-technical team members can modify what data they send to different tools without needing engineering support.",
+      imageSrc: "/images/home/integrations/mailjet/grouparoo-to-mailjet.png",
+      imageAlt: "Map data to Mailjet",
+      imageWidth: 1172,
+      imageHeight: 645,
+    },
+  ],
+  otherPluginsHeading:
+    "Import your customer data from these tools into Mailjet",
 };
