1b6bb5cf36b1a13ae682be60924f185c38e02027
Teal Larson <teal.larson@grouparoo.com>
2022-02-08 16:23:44 -0500

    Get started page update

---
+++ components/getStarted/GetStartedCard.tsx
@@ -0,0 +1,49 @@
+import { Card, Button, Row } from "react-bootstrap";
+import Image from "../Image";
+
+interface GetStartedCardProps {
+  title: string;
+  image: string;
+  imageWidth: string;
+  tagline: string;
+  buttonVariant: string;
+  buttonText: string;
+  footerText?: string;
+}
+export const GetStartedCard = ({
+  title,
+  image,
+  imageWidth,
+  tagline,
+  buttonVariant,
+  buttonText,
+  footerText,
+}: GetStartedCardProps) => {
+  return (
+    <Card className="mx-3 pt-4 pb-5 shadow-lg h-100 mb-4" bg="white">
+      <Card.Header className="bg-white border-0">
+        <Image src={image} width={imageWidth} height="63"></Image>
+        <Card.Title>
+          <div className="h3 pt-2 mb-2">{title}</div>
+        </Card.Title>
+      </Card.Header>
+      <Card.Body className="lead align-middle">{tagline} </Card.Body>
+      <Card.Footer className="border-0 bg-white">
+        <Row>
+          <Button
+            variant={buttonVariant}
+            href="/docs/getting-started"
+            className="col-4 rounded-pill mx-auto"
+          >
+            {buttonText}
+          </Button>
+          {footerText ? (
+            <span className="small mt-4">{footerText}</span>
+          ) : (
+            <div className="mb-5"></div>
+          )}
+        </Row>{" "}
+      </Card.Footer>
+    </Card>
+  );
+};
--- pages/get-started.tsx
+++ pages/get-started.tsx
@@ -1,6 +1,7 @@
 import { Container, Button, Row, Col, Card } from "react-bootstrap";
 import Link from "next/link";
 import SEO from "../components/SEO";
+import { GetStartedCard } from "../components/getStarted/GetStartedCard";
 
 export default function GetStarted({ props }) {
   const title = "Grouparoo: Get started";
@@ -23,63 +24,39 @@ export default function GetStarted({ props }) {
           <Row className="pt-5 pb-3">
             <Col>
               <h1 className="pt-1 pb-3">Sync data how you want</h1>
-              <h2 className="pb-3 pb-xl-5">
-                Use Grouparoo to power your Reverse ETL pipelines and workflows
-              </h2>
+              <p className="big pb-3 pb-xl-5">
+                Use Grouparoo to power your Reverse ETL pipelines and workflows.
+              </p>
             </Col>
           </Row>
           <Row className="d-flex justify-content-center">
             <Col className="mb-4 col-12 col-xl-5 col-md-7">
-              <Card
-                className="pricingCard mx-3 pt-3 pb-5 shadow-lg h-100 mb-4"
-                bg="white"
-              >
-                <Card.Header className="bg-white border-0">
-                  <Card.Title>
-                    <div className="h3 pt-2 mb-2">Self-Hosted</div>
-                    <hr className="col-9 mx-auto" />
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body className="lead align-middle">
-                  Grouparoo is open source and free to run in your own cloud
-                </Card.Body>
-                <Card.Footer className="border-0 bg-white">
-                  <Button
-                    variant="outline-primary"
-                    href="/docs/getting-started"
-                    className="col-4"
-                  >
-                    Learn more
-                  </Button>
-                </Card.Footer>
-              </Card>
+              <GetStartedCard
+                title="Self-Hosted"
+                image="/images/home/get-started/server.svg"
+                imageWidth="64"
+                tagline="Grouparoo is open source and free to run in your own cloud."
+                buttonVariant="outline-dark"
+                buttonText="Learn More"
+              />
             </Col>
             <Col className="mb-4 col-12 col-xl-5 col-md-7">
-              <Card
-                className="pricingCard mx-3 pt-3 pb-4 shadow-lg h-100 mb-4"
-                bg="white"
-              >
-                <Card.Header className="bg-white border-0">
-                  <Card.Title>
-                    <div className="h3 pt-2  mb-2">Cloud</div>
-                    <hr className="col-9 mx-auto" />
-                  </Card.Title>
-                </Card.Header>
-                <Card.Body className="lead">
-                  Get syncing from databases to SaaS tools right away.
-                </Card.Body>
-                <Card.Footer className="border-0 bg-white">
-                  <Button variant="primary" href="/trial" className="col-4">
-                    Start free trial
-                  </Button>
-                </Card.Footer>
-                <small>No credit card required</small>
-                <Link href="/pricing">
-                  <a className="my-3">View pricing plans</a>
-                </Link>
-              </Card>
+              <GetStartedCard
+                title="Cloud"
+                image="/images/home/get-started/cloud-cycle.svg"
+                imageWidth="80"
+                tagline="Get syncing from databases to SaaS tools right away."
+                buttonVariant="secondary"
+                buttonText="Start Free Trial"
+                footerText="No credit card required"
+              />
             </Col>
           </Row>
+          <Link href="/pricing">
+            <p className="my-2 text-black">
+              <u>View pricing plans</u>
+            </p>
+          </Link>
         </Container>
       </div>
     </>
