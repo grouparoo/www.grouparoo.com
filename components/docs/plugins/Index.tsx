5f08d22aca9bc6c169c2eed5be60025a5d8c7e7f
Teal Larson <teal.larson@grouparoo.com>
2022-02-15 15:05:04 -0500

    Remove unused components now that we don't have `grouparoo generate` (#925)
    * Remove unused components now that we don't have `grouparoo generate`
    
    * remove createApp

--- components/docs/plugins/Index.tsx
+++ components/docs/plugins/Index.tsx
@@ -1,17 +1,9 @@
-import PluginDocsCalculatedPropertySource from "./CalculatedPropertySource";
-import PluginDocsColumnarDestination from "./ColumnarDestination";
-import PluginDocsColumnarSource from "./ColumnarSource";
-import PluginDocsCreateApp from "./CreateApp";
 import PluginDocsInstallation from "./Installation";
 import PluginDocsIntegrationNotes from "./IntegrationNotes";
 import PluginDocsLinks from "./Links";
 import OptionsList from "./OptionsList";
 
 export {
-  PluginDocsCalculatedPropertySource,
-  PluginDocsColumnarDestination,
-  PluginDocsColumnarSource,
-  PluginDocsCreateApp,
   PluginDocsInstallation,
   PluginDocsIntegrationNotes,
   PluginDocsLinks,
