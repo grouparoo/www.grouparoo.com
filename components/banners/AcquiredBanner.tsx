e5210b14548907ee7f5fed3b270e94a2d899c3a6
Brian Leonard <brian@bleonard.com>
2022-04-07 10:12:32 -0700

    prettier

--- components/banners/AcquiredBanner.tsx
+++ components/banners/AcquiredBanner.tsx
@@ -2,17 +2,12 @@ import { Alert } from "react-bootstrap";
 import Link from "next/link";
 
 export function AcquiredBanner() {
-
   return (
     <Alert variant="success" style={{ textAlign: "center", margin: 0 }}>
       <small>
         Grouparoo has been acquired by Airbyte.{" "}
         <strong>
-          <Link
-            href="/blog/grouparoo-acquired-by-airbyte"
-          >
-            Learn more
-          </Link>
+          <Link href="/blog/grouparoo-acquired-by-airbyte">Learn more</Link>
         </strong>{" "}
       </small>
     </Alert>
